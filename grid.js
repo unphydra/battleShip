20fae913eb25e72d412ad3d87e6281b3d923c16b
rivu <58026024+unphydra@users.noreply.github.com>
2020-01-28 16:14:29 +0530

    numbering and alphabets are added to the side box

--- grid.js
+++ grid.js
@@ -1,14 +1,32 @@
 const row = 11;
 const col = 11;
 
+const alphabets = [
+  '&nbsp;',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j'
+];
+
 const getCellId = function(col, row) {
   return `${col}_${row}`;
 };
 
 const createCell = function(player, col, row) {
-  console.log(player, col, row);
-
   const cell = document.createElement('div');
+  if (row === 0) {
+    cell.innerText = col;
+  }
+  if (col === 0) {
+    cell.innerHTML = alphabets[row];
+  }
   cell.className = 'cell';
   cell.id = getCellId(col, row);
   player.appendChild(cell);
@@ -19,8 +37,8 @@ const displayGrid = function() {
   const player2 = document.getElementById('player2');
   const players = [player1, player2];
   players.forEach(player => {
-    for (let x = 1; x <= col; x++) {
-      for (let y = 1; y <= row; y++) {
+    for (let y = 0; y < row; y++) {
+      for (let x = 0; x < col; x++) {
         createCell(player, x, y);
       }
     }
